52c5f0f380bf1dbd17a044a16dd741161fea7e4f
Bunyan33 <sam.ally33@gmail.com>
2024-04-09 02:28:35 +0530

    Data Seeding

---
+++ MaxiShop.Infrastructure/Common/SeedData.cs
@@ -0,0 +1,54 @@
+﻿using MaxiShop.Domine.Models;
+using MaxiShop.Infrastructure.DbContexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxiShop.Infrastructure.Common
+{
+    public class SeedData
+    {
+        public static async Task SeedDataAsync(ApplicationDbContext _dbContext)
+        {
+            if(!_dbContext.Brands.Any())
+            {
+                await _dbContext.AddRangeAsync(
+                    
+                    new Brand
+                    {
+                        Name = "Apple",
+                        EstablishedYear = 1988
+                    },
+                    new Brand
+                    {
+                        Name = "Samsung",
+                        EstablishedYear = 1958
+                    },
+                    new Brand
+                    {
+                        Name = "Hp",
+                        EstablishedYear = 1988
+                    },
+                    new Brand
+                    {
+                        Name = "Sony",
+                        EstablishedYear = 1988
+                    },
+                    new Brand
+                    {
+                        Name = "Lenovo",
+                        EstablishedYear = 1988
+                    },
+                    new Brand
+                    {
+                        Name = "Acer",
+                        EstablishedYear = 1988
+                    });
+
+                await _dbContext.SaveChangesAsync();
+            }
+        }
+    }
+}
--- MaxiShop.Web/Program.cs
+++ MaxiShop.Web/Program.cs
@@ -2,6 +2,7 @@ using MaxiShop.Infrastructure;
 using MaxiShop.Application;
 using MaxiShop.Infrastructure.DbContexts;
 using Microsoft.EntityFrameworkCore;
+using MaxiShop.Infrastructure.Common;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -35,7 +36,36 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+#region Config for Seeding Data to Database
+static async void UpdateDataBaseAsync(IHost host)
+{
+    using(var scope = host.Services.CreateScope())
+    {
+        var service = scope.ServiceProvider;
+
+        try
+        {
+            var context = service.GetRequiredService<ApplicationDbContext>();
+
+            if (context.Database.IsSqlServer())
+            {
+                context.Database.Migrate();
+            }
+
+            await SeedData.SeedDataAsync(context);
+        }
+        catch (Exception ex)
+        {
+            var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+            logger.LogError(ex, "An error while seeding the database");
+        }
+    }
+}
+#endregion
+
 var app = builder.Build();
+
+UpdateDataBaseAsync(app);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
